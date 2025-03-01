64d709045b58ef3cfa0dd638f371662ee582e5fd
joaolucas14 <jlparceiro10@gmail.com>
2025-03-01 12:31:53 -0300

    adicionando a propriedade clone_url à interface IRepositorio e estilizando a página de repositórios

--- src/Interfaces/IRepositorio.ts
+++ src/Interfaces/IRepositorio.ts
@@ -2,6 +2,7 @@ interface IRepositorio {
   id: number;
   name: string;
   stargazers_count: number;
+  clone_url: string;
 }
 
 export default IRepositorio;
--- src/routes/Repositorios/index.tsx
+++ src/routes/Repositorios/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import useRepositorios from "../../state/hooks/useRepositorios";
 import useUser from "../../state/hooks/useUser";
+import styles from "./Repositorios.module.css";
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Repositorios() {
   const { buscaRepositorio, repositorios } = useRepositorios();
@@ -16,18 +19,30 @@ export default function Repositorios() {
   }, [hasFetched]);
   return (
     <div>
-      <h1>Repositorios Favoritos</h1>
-      {repositorios &&
-        repositorios
-          .filter((rep) => rep.stargazers_count > 0)
-          .sort((a, b) => b.stargazers_count - a.stargazers_count)
-          .slice(0, 5)
-          .map((rep) => (
-            <div key={rep.id}>
-              <p>{rep.name}</p>
-              <p>{rep.stargazers_count.toLocaleString("pt-BR")}</p>
-            </div>
-          ))}
+      <h1>Melhores Projetos de {user?.login}</h1>
+      <div className={styles.container}>
+        {repositorios &&
+          repositorios
+            .filter((rep) => rep.stargazers_count > 0)
+            .sort((a, b) => b.stargazers_count - a.stargazers_count)
+            .slice(0, 5)
+            .map((rep) => (
+              <a href={rep.clone_url} target="_blank">
+                <div key={rep.id} className={styles.repositorio}>
+                  <p>{rep.name}</p>
+                  <p>
+                    <span>
+                      <FaStar />
+                    </span>
+                    {rep.stargazers_count.toLocaleString("pt-BR")}
+                  </p>
+                </div>
+              </a>
+            ))}
+        <Link to={"/"} className={styles.link}>
+          Voltar
+        </Link>
+      </div>
     </div>
   );
 }
